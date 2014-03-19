d546b2bf7c6b8592b20d4e8fa9e7365598666a50
micataudella <michele.cataudella@gmail.com>
2014-03-19 19:28:58 +0100

    m

---
+++ socialstudy android/src/socialstudy/android/LoginActivity.java
@@ -0,0 +1,64 @@
+package socialstudy.android;
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class LoginActivity extends CordovaActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 
+//		 setContentView(R.layout.login);
+//		 WebView wv = (WebView) findViewById(R.id.webView1);
+//		 wv.loadUrl("https://studisocial.appspot.com/");
+		 
+	        super.init();
+	         //Set by <content src="index.html" /> in config.xml
+	        //super.loadUrl(Config.getStartUrl());
+	        
+	        super.loadUrl("https://studisocial.appspot.com/");
+
+		
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.login, menu);
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
+//
+//	/**
+//	 * A placeholder fragment containing a simple view.
+//	 */
+//	public static class PlaceholderFragment extends Fragment {
+//
+//		public PlaceholderFragment() {
+//		}
+//
+//		@Override
+//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//				Bundle savedInstanceState) {
+//			View rootView = inflater.inflate(R.layout.fragment_login,
+//					container, false);
+//			return rootView;
+//		}
+//	}
+
+}
